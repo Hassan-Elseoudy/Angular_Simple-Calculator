d644d761a5379f5f6278fdad18c7d855ed776ebb
Hassan Elseoudy <hassan.elseoudy@sia-world.com>
2020-07-17 14:24:46 +0200

    Controllers are done

---
+++ src/app/app-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+  * This function is used to get 2 numbers and add them upon click
+  * 
+  * @param firstNumber First Number to be added.
+  * @param secondNumber Second Number to be added.
+  */
+
+  addTwoNumbers(firstNumber: number, secondNumber: number): Observable<Object> {
+    return this.http.get(`http://localhost:3000/api/calc?firstNumber=${firstNumber}&secondNumber=${secondNumber}`);
+  }
+
+  /**
+   * This function is used when we want to average some numbers, we should get them upon click
+   * @param numbers array of numbes to be averaged.
+   */
+
+  averageNumbers(numbers: Array<number>): Observable<Object> {
+    return this.http.post(`http://localhost:3000/api/calc`, { numbers });
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppService } from './app-service.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CalculatorApp';
+  
+  addResult = '0';
+  avgResult = '0';
+
+
+  constructor(private appService: AppService, private toastr: ToastrService) {
+  }
+
+
+  clickOnAddButton() {
+    this.appService.addTwoNumbers(8, 10).subscribe((response: any) => {
+      if (response.status === 200) {
+        this.toastr.success('Success!');
+        this.addResult = response.result;
+      } else {
+        this.toastr.error('Error!');
+        this.addResult = '0';
+      }
+    });
+  }
+
+  clickOnAverageButton() {
+    this.appService.averageNumbers([1, 2, 3, 4, 5]).subscribe((response: any) => {
+      console.log(response);
+      if (response.status === 200) {
+        this.toastr.success('Success!');
+        this.avgResult = response.result;
+      } else {
+        this.toastr.error('Error!');
+        this.avgResult = '0';
+      }
+    });
+  }
+
+
 }
